c5edcb288a5aa4e1e292bf6f381c688343eabe63
xiaoguo <guox652@gmail.com>
2013-06-15 14:28:48 -0400

    Added votes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130615173657) do
+ActiveRecord::Schema.define(:version => 20130615175455) do
 
   create_table "topics", :force => true do |t|
     t.string   "title"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(:version => 20130615173657) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.text     "about"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "votes", :force => true do |t|
+    t.integer  "topic_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
